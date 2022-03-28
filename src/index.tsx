23fcfec02d70fb4953c759a1a9e7a54046203c70
Glenn Robert Strandos <strandos.glenn@gmail.com>
2022-03-28 20:43:15 +0200

    changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
